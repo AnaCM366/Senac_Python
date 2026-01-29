6730ab6c433fa159b855af801fe5c84e04770818
AnaCM366 <anacarolinamachado366@gmail.com>
2026-01-28 21:57:46 -0300

    Continuação da Aula 4, essa é a Aula 4.1, acrescentando bootstrap e visualização das listas de chamados

--- Aula_4/core/views.py
+++ Aula_4/core/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("<h1>Hello, world!</h1><p>Meu primeiro Django está online.</p>")
@@ -9,20 +10,14 @@ chamados = [
 ]
 
 def listar(request):
-    # Lógica para listar os chamados em HTML
-    html = "<h1> HelpDesk - Lista de Chamados</h1><hr>"
-
-    for i, c in enumerate(chamados):
-        html += f"<p>ID: {i} | <b>{c['lab']}</b> - {c['problema']} {c['prioridade']}</p>"
-
-    html += "<br><a href='/novo/Lab02/Teclado/Alta/'>[Simulador Novo Chamado]</a>"
-    return HttpResponse(html)
+    return render(request, "core/listar.html", {"chamados": chamados})
 
 def criar(request, lab, problema, prioridade):
     # Criando o dicionário e adicionando à lista
     novo = {
         "lab": lab,
-        "problema": problema
+        "problema": problema,
+        "prioridade": prioridade
     }
     chamados.append(novo)
 
